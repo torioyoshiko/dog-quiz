22e638093bd396f169dfa8a1009b6af15bc15215
Torio Yoshiko <torio.yoshiko@gmail.com>
2020-02-20 21:16:17 +0300

    Initial commit

---
+++ src/components/start.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/start.css'
+
+function Start() {
+    return(
+        <div className='background'>
+            <h2>Who wants to be a dogellionaire?</h2>
+            <p>
+                It’s a quiz which will test your knowledge of dog breeds.
+                You will be given an image of a dog and 4 answers.
+                Let’s see how good are you at knowing your good boys and girls
+            </p>
+            <button>Start</button>
+        </div>
+    )
+}
+
+export default Start;
