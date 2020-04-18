6a4e682465b339a4123437e58fcbbf9c363b5e34
Torio Yoshiko <torio.yoshiko@gmail.com>
2020-04-18 18:13:52 +0200

    Updates

---
+++ precache-manifest.7872ec612beb69b58878e200f5e0604a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404a4f20ec14e7d89d6a86fd36f6a71d",
+    "url": "/dog-quiz/index.html"
+  },
+  {
+    "revision": "c4fc089e4ab9b3c13b11",
+    "url": "/dog-quiz/static/css/main.d96dad5f.chunk.css"
+  },
+  {
+    "revision": "f41a9239d0fee7707643",
+    "url": "/dog-quiz/static/js/2.b8f9bdff.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/dog-quiz/static/js/2.b8f9bdff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fc089e4ab9b3c13b11",
+    "url": "/dog-quiz/static/js/main.c2caad2e.chunk.js"
+  },
+  {
+    "revision": "ed2cbbbd3c084c7def09",
+    "url": "/dog-quiz/static/js/runtime-main.c121bc0e.js"
+  }
+]);
